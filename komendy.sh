b638f2d116e7816ba5d2350dd5572e6421e68ee5
Jakub <cubus331122@gmail.com>
2023-03-30 14:38:20 +0200

    tutaj sa komendy

---
+++ komendy.sh
@@ -0,0 +1,14 @@
+https://github.com/cubus312/lab3
+
+git commit, git commit--ammend
+git branch
+git checkout, git checkout -b
+git merge
+git rebase, git rebase -i
+git branch -f
+git reset, git reset --hard
+git revert
+git cherry-pick
+git tag
+git push
+git pull
